b3c2c2b86c5218ecd6ceac0f351d03310a0c1ea7
balckpixie <kuropixie@gmail.com>
2025-07-29 15:28:36 +0000

    各種メソッドを追加

--- lib/index.ts
+++ lib/index.ts
@@ -1,44 +1,97 @@
 import videojs from 'video.js';
-import initializeThumbnailSprite from './utils/initializeThumbnailSprite';
+import sortSprites from './utils/sortSprites';
+import checkOverlap from './utils/checkOverlap';
+import checkOptions from './utils/checkOptions';
+import generatePreview from './utils/generatePreview';
 
 const Plugin = videojs.getPlugin('plugin');
 
 class ThumbnailSprite extends Plugin {
-  private options: ThumbnailSprite.Options;
+  private sprites: ThumbnailSprite.Sprite[] = [];
+  private controls?: ThumbnailSprite.IIndexableComponent;
+  private progressCtrl?: ThumbnailSprite.IIndexableComponent;
 
   constructor(player: videojs.Player, options?: ThumbnailSprite.Options) {
     super(player);
-    const emptyOptions: ThumbnailSprite.Options = {
-      sprites: [],
-    }
-    this.options = (options !== undefined) ? options : emptyOptions;
-    
-    // When player instance is ready, initialize the plugin
+
+    const sprites = options?.sprites || [];
+
+    // 先に player にメソッドをバインド
+    (player as any).updateThumbnailSprites = (opts: ThumbnailSprite.Options) => {
+      if (opts?.sprites) this.setupSprites(opts.sprites);
+    };
+
+    (player as any).resetThumbnailSprites = () => {
+      this.removeEventHandlers();
+      player.removeClass('vjs-sprite-thumbnails');
+      const previewEl = player.el().querySelector('.vjs-thumbnail-preview');
+      if (previewEl?.parentNode) {
+        previewEl.parentNode.removeChild(previewEl);
+      }
+      this.sprites = [];
+    };
+
+    (player as any).hasThumbnailSprites = () => {
+      return this.sprites.length > 0;
+    };
+
+    // DOMがreadyになったらUI部品にアクセス
     this.player.ready(() => {
-      initializeThumbnailSprite(
-        this.player,
-        this.options,
-      );
+      if (!player.controlBar) return;
+
+      this.controls = player.controlBar as ThumbnailSprite.IIndexableComponent;
+      if (!this.controls['progressControl']) return;
+
+      this.progressCtrl = this.controls['progressControl'] as ThumbnailSprite.IIndexableComponent;
+
+      if (sprites.length > 0) {
+        this.setupSprites(sprites);
+      }
     });
+  }
+
+  private setupSprites(sprites: ThumbnailSprite.Sprite[]) {
+    this.sprites = sprites;
+
+    sortSprites(this.sprites);
+    checkOverlap(this.sprites);
+    checkOptions(this.sprites);
+
+    this.removeEventHandlers();
+    this.addEventHandlers();
+
+    this.player.addClass('vjs-sprite-thumbnails');
+  }
+
+  private addEventHandlers() {
+    if (!this.progressCtrl) return;
+    const handler = () => generatePreview(this.player, this.controls!, this.sprites);
+    this.progressCtrl.on('mousemove', handler);
+    this.progressCtrl.on('touchmove', handler);
+  }
+
+  private removeEventHandlers() {
+    if (!this.progressCtrl) return;
+    this.progressCtrl.off('mousemove');
+    this.progressCtrl.off('touchmove');
   }
 }
 
 videojs.registerPlugin('thumbnailSprite', ThumbnailSprite);
 
 namespace ThumbnailSprite {
-  // 옵션에 있어 글로벌 설정
-    // 이후 스타일 추가 등으로 확장 가능성
   export interface Options {
     sprites: Array<Sprite>;
   }
+
   export interface Sprite {
-    url: string;      // thumbnail sprite's url
-    start: number;    // start timestamp of this sprite in video
-    duration: number; // duration of this sprite in video
-    width: number;    // width of each preview image
-    height: number;   // height of each preview image
-    interval: number; // interval of each preview images in sprite
-  };
+    url: string;
+    start: number;
+    duration: number;
+    width: number;
+    height: number;
+    interval: number;
+  }
 
   export interface IIndexableComponent extends videojs.Component {
     [key: string]: any;
